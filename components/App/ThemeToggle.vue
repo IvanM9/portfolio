49bfc3734f3e38e9f2cd124aef8d703219c0e789
Iván Manzaba <66442822+IvanM9@users.noreply.github.com>
2024-01-02 18:56:01 +0000

    cambios

--- components/App/ThemeToggle.vue
+++ components/App/ThemeToggle.vue
@@ -12,7 +12,7 @@ const isDark = computed({
 </script>
 
 <template>
-  <UTooltip text="Toggle theme" :ui="{ popper: { strategy: 'absolute' } }">
+  <UTooltip text="Alternar tema" :ui="{ popper: { strategy: 'absolute' } }">
     <button
       class="relative px-3 py-4 flex items-center justify-center transition hover:text-primary-500 dark:hover:text-primary-400"
       @click="isDark = !isDark"
