8e803c64cc81ad99fa7917a5a280364208085698
Iván Manzaba <mauricio.9.inm@gmail.com>
2024-01-14 19:51:02 -0500

    cambio de foto

--- pages/projects.vue
+++ pages/projects.vue
@@ -13,7 +13,7 @@
 
 <script setup>
 const description =
-  "He trabajado en varios de pequeños proyectos a lo largo de los años, pero estos son de los que estoy más orgulloso. Muchos de ellos son de código abierto, por lo que si ve algo que despierta su interés, consulte el código y contribuya si tiene ideas sobre cómo mejorarlo.";
+  "He trabajado en varios proyectos a lo largo de los años, pero estos son de los que estoy más orgulloso. Muchos de ellos son de código abierto, por lo que si ve algo que despierta su interés, consulte el código y contribuya si tiene ideas sobre cómo mejorarlo.";
 useSeoMeta({
   title: "Proyectos | Iván Manzaba",
   description,
