b6f76c064c45577498e211585a4501c85198b154
Iván Manzaba <mauricio.9.inm@gmail.com>
2024-09-27 13:35:48 -0500

    Remove unused software and hardware configurations

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -48,17 +48,9 @@ const items = [
     path: "/projects",
     icon: "solar:folder-with-files-outline",
   },
-/*   {
-    name: "Articles",
-    path: "/articles",
-    icon: "solar:document-add-outline",
-  },
+/*  
   { name: "Lab", path: "/lab", icon: "heroicons:beaker" },
-  {
-    name: "What's in my bag?",
-    path: "/whats-in-my-bag",
-    icon: "solar:backpack-outline",
-  }, */
+  */
   // {
   //   name: "Bookmarks",
   //   path: "/bookmarks",
